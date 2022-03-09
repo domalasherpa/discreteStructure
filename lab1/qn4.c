ec439451fb73030e3dd68e2054a012eda7130610
domalasherpa <domalasherpa9813@gmail.com>
2022-03-09 20:23:16 +0545

    set operation added

--- lab1/qn4.c
+++ lab1/qn4.c
@@ -1,11 +1,68 @@
 #include <stdio.h>
 #define n 5
 
+
+void fuzzyunion(char set[][20], float deg[][2]){
+	int first = 1, i;
+	for(i = 0; i < n; i++){
+		if(deg[i][0] > deg[i][1]){
+			if(first){
+				printf(" (%s, %.2f) ", set[i], deg[i][0]);
+				first = 0;
+			}
+			else{
+				printf(", (%s, %.2f) ", set[i], deg[i][0]);
+
+			}
+		}
+		else{
+			if(first){
+				printf(" (%s, %.2f) ", set[i], deg[i][1]);
+				first = 0;
+			}
+			else{
+				printf(", (%s, %.2f) ", set[i], deg[i][1]);
+
+			}
+				
+		}
+	
+	}
+}
+
+
+void fuzzyinter(char set[][20], float deg[][2]){
+	int first = 1, i;
+	for(i = 0; i < n; i++){
+			if(deg[i][0] < deg[i][1]){
+				if(first){
+					printf(" (%s, %.2f) ", set[i], deg[i][0]);
+					first = 0;
+				}
+				else{
+					printf(", (%s, %.2f) ", set[i], deg[i][0]);
+
+				}
+			}
+			else{
+				if(first){
+					printf(" (%s, %.2f) ", set[i], deg[i][1]);
+					first = 0;
+				}
+				else{
+					printf(", (%s, %.2f) ", set[i], deg[i][1]);
+
+				}
+			}
+	
+	}
+}
+
 int main(){
 	
 	char set[n][20];
 	int age[n][1];
-	float deg[n][1];
+	float deg[n][2];
 	int i;
 	
 	for( i = 0 ; i< n; i++){
@@ -40,13 +97,13 @@ int main(){
 	
 	for( i = 0; i < n; i++){
 		if(age[i][0] <= 15){
-			deg[i][0] = 1;
+			deg[i][1] = 1;
 		}
 		else if(age[i][0] <= 30){
-			deg[i][0] = (35 - age[i][0])/20.0;
+			deg[i][1] = (35 - age[i][0])/20.0;
 		}
 		else{
-			deg[i][0] = 0;
+			deg[i][1] = 0;
 		}
 		
 	}
@@ -56,16 +113,23 @@ int main(){
 	printf("{ ");
 	for(i = 0; i< n; i++){
 		if(i != n - 1){
-			printf("(%s, %.2f), ", set[i], deg[i][0]);
+			printf("(%s, %.2f), ", set[i], deg[i][1]);
 		}
 		else{
-			printf("(%s, %.2f) ", set[i], deg[i][0]);
+			printf("(%s, %.2f) ", set[i], deg[i][1]);
 		}
 		
 	}
 	printf("} ");
-	
-	
+
+
+	printf("\nUnion: {");
+	fuzzyunion(set, deg);
+	printf("} \n");
+
+	printf("\nIntersection: {");
+	fuzzyinter(set, deg);
+	printf("} \n");
 	
 	return 0;
 }
