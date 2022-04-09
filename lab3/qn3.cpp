3997ff1933827c50db9e356be1f25b70e192c01f
domalasherpa <domalasherpa9813@gmail.com>
2022-04-09 06:46:07 +0545

    Hyposthesis and Conclusion

--- lab3/qn3.cpp
+++ lab3/qn3.cpp
@@ -55,37 +55,38 @@ void hypothesis(int *first, int *sec, int *third, int p, int q, int r){
 
 int main(){
 
-    /*
-        I will buy a new goat  = p
-        I will buy a new Yugo = q
-        I will need a loan    = r
+/*
+    I will buy a new goat  = p
+    I will buy a new Yugo = q
+    I will need a loan    = r
 
-        p V q
-        p n q -> r
-        q n ~r
+    p V q
+    p n q -> r
+    q n ~r
 
-        therfore ~p
-    */
-        int p[2] = {0, 1};
-        int q[2] = {0, 1};
-        int r[2] = {0, 1};
+    therfore ~p
+*/
+    int p[2] = {0, 1};
+    int q[2] = {0, 1};
+    int r[2] = {0, 1};
 
-        cout<<"P\tQ\tR\t";
-        cout << "P V Q\t" << "(P n Q) -> R\t" << "Q n ~R\t" ;
-        cout << "Conclusion\n";
-        cout << "---------------------------------------------------------------------\n";
-        for(int i = 0; i < 2; i++){
-            for(int j = 0; j < 2; j++){
-                for(int k = 0; k < 2; k++){
-                    int first , sec, third;
-        
-                    print(p[i]); cout <<"\t";
-                    print(q[j]); cout << "\t";
-                    print(r[k]); cout << "\t";
-                    hypothesis(&first, &sec, &third, p[i], q[j], r[k]);
-                    proof(first, sec, third);
-                    
-                }
+    cout<<"P\tQ\tR\t";
+    cout << "P V Q\t" << "(P n Q) -> R\t" << "Q n ~R\t" ;
+    cout << "Conclusion\n";
+    cout << "---------------------------------------------------------------------\n";
+    for(int i = 0; i < 2; i++){
+        for(int j = 0; j < 2; j++){
+            for(int k = 0; k < 2; k++){
+                int first , sec, third;
+    
+                print(p[i]); cout <<"\t";
+                print(q[j]); cout << "\t";
+                print(r[k]); cout << "\t";
+
+                hypothesis(&first, &sec, &third, p[i], q[j], r[k]);
+                proof(first, sec, third);
+                
+            }
         }
     }
     return 0;
